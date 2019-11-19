4cef2bba8355231492ba495624e96c7713fe6917
Qingyang Wu <wilwu@ucdavis.edu>
2019-11-19 03:54:18 -0800

    fix argument utils

--- torchfly/training/arguments/utils.py
+++ torchfly/training/arguments/utils.py
@@ -1,7 +1,7 @@
 import argparse
 
 def args_dict_to_list(args):
-    res = " ".join([ "--" + k + " " + str(v) for k, v in args.items()])
-    res = res.split()
+    res = "\t".join([ "--" + k + "\t" + str(v) for k, v in args.items()])
+    res = res.split("\t")
     res = [item for item in res if item != 'None']
     return res
